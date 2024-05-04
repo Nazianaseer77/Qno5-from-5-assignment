5bbb3a84074c42a77152c55755e8b2580ec97e55
Nazianaseer77 <160523698+Nazianaseer77@users.noreply.github.com>
2024-05-04 23:53:43 +0500

    Qno5 from 5 assignment

---
+++ app.js
@@ -0,0 +1,8 @@
+//write a function that return the square of a number.
+function getSquare(num) {
+    return Math.pow(num, 2);
+}
+console.log(getSquare(5));
+console.log(getSquare(10));
+console.log(getSquare(15));
+console.log(getSquare(20));
---
+++ app.ts
@@ -0,0 +1,9 @@
+//write a function that return the square of a number.
+
+function getSquare(num:number){
+    return num ** 2;
+}
+ console.log(getSquare(5));
+ console.log(getSquare(10));
+ console.log(getSquare(15));
+ console.log(getSquare(20));
